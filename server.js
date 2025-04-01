bcd46a261f4fca318e0fbab4945d7eb92efd964d
mohamedsherif301 <myosre122@gmail.com>
2025-04-01 10:50:36 +0200

    changes Server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('db.json');  // تأكد أن db.json موجود
 const middlewares = jsonServer.defaults();
 
+// استخدم الـ middleware الافتراضية (مثل الـ logging و الـ CORS)
 server.use(middlewares);
+
+// إضافة الـ router
 server.use(router);
 
-const PORT = process.env.PORT || 5000;
+// تحديد الـ port
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`JSON Server is running on port ${PORT}`);
 });
